6b4cfa376a5dda6c6a7ab905e19cc591485d444a
manos <emmanouil.psanis@gmail.com>
2025-03-01 01:02:33 +0100

    dev: hide info message after 5 secs

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,5 +1,7 @@
-var message_timeout = document.getElementById("message-timer");
+let message_timeout = document.getElementById("message-timer");
 
 setTimeout(function () {
-    message_timeout.style.display = "none";
+    if (message_timeout) {
+        message_timeout.style.display = "none";
+    }
 }, 5000);
