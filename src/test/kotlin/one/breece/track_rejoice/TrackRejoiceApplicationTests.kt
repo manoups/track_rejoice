2d30029146f9cde5516cc6ebaaf096e75a192ebf
manos <emmanouil.psanis@gmail.com>
2025-03-14 18:43:13 +0100

    dev: integrate external libs

--- src/test/kotlin/one/breece/track_rejoice/TrackRejoiceApplicationTests.kt
+++ src/test/kotlin/one/breece/track_rejoice/TrackRejoiceApplicationTests.kt
@@ -2,12 +2,24 @@ package one.breece.track_rejoice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.modulith.core.ApplicationModules
+import org.springframework.modulith.docs.Documenter
 
 @SpringBootTest
 class TrackRejoiceApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun verifiesModularStructure() {
+        val modules: ApplicationModules = ApplicationModules.of(TrackRejoiceApplication::class.java)
+        modules.verify()
+    }
+
+    @Test
+    fun createModuleDocumentation() {
+        val modules: ApplicationModules = ApplicationModules.of(TrackRejoiceApplication::class.java)
+        Documenter(modules)
+            .writeDocumentation()
+            .writeIndividualModulesAsPlantUml()
     }
 
 }
