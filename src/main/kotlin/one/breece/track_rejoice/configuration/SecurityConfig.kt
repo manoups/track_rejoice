a4be74122dafd40208e6f623ed2ac5ebd847b34d
manos <emmanouil.psanis@gmail.com>
2025-03-23 12:30:30 +0100

    dev: open access to login/oauth2/code/google path

--- src/main/kotlin/one/breece/track_rejoice/configuration/SecurityConfig.kt
+++ src/main/kotlin/one/breece/track_rejoice/configuration/SecurityConfig.kt
@@ -45,6 +45,7 @@ class SecurityConfig {
         http {
             csrf { disable() }
             authorizeHttpRequests {
+                authorize("/login/**", permitAll)
                 authorize("/css/**", permitAll)
                 authorize("/js/**", permitAll)
                 authorize("/user/**", hasAuthority("ROLE_USER"))
@@ -73,7 +74,8 @@ class SecurityConfig {
             }
             oauth2Login {
                 loginPage = "/login"
-                defaultSuccessUrl("/", true) }
+                defaultSuccessUrl("/", true)
+            }
             logout {
                 clearAuthentication = true
                 invalidateHttpSession = true
@@ -91,15 +93,6 @@ class SecurityConfig {
         return expressionHandler
     }
 
-    /* @Bean
-     fun authProvider(userRepository: UserRepository, userDetailsService: UserDetailsService): DaoAuthenticationProvider {
-         val authProvider = CustomAuthenticationProvider(userRepository)
-         authProvider.setUserDetailsService(userDetailsService)
-         authProvider.setPasswordEncoder(passwordEncoder())
- //        authProvider.setPostAuthenticationChecks(differentLocationChecker())
-         return authProvider
-     }*/
-
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder(11)
@@ -116,18 +109,6 @@ class SecurityConfig {
             CustomRememberMeServices("theKey", userDetailsService, InMemoryTokenRepositoryImpl(), userRepository)
         return rememberMeServices
     }
-
-    /*    @Bean(name = ["GeoIPCountry"])
-        @Throws(IOException::class)
-        fun databaseReader(): DatabaseReader {
-            val resource = File(
-                javaClass
-                    .classLoader
-                    .getResource("maxmind/GeoLite2-Country.mmdb")
-                    .file
-            )
-            return DatabaseReader.Builder(resource).build()
-        }*/
 
     @Bean
     fun roleHierarchy(): RoleHierarchy {
@@ -162,129 +143,4 @@ class SecurityConfig {
             userDetails
         }
     }
-
-    /*@Bean
-    fun userDetailsService(): InMemoryUserDetailsManager {
-        val user1: UserDetails = User.withUsername("user")
-            .password(passwordEncoder().encode("user"))
-            .roles("USER")
-            .build()
-        val admin: UserDetails = User.withUsername("admin")
-            .password(passwordEncoder().encode("admin"))
-            .roles("ADMIN", "USER")
-            .build()
-        return InMemoryUserDetailsManager(user1, admin)
-    }*/
 }
-//    @Autowired
-//    private lateinit var oauthUserService: CustomOAuth2UserService
-
-//    @Bean
-//    @Throws(Exception::class)
-//    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
-//        http.authorizeHttpRequests().anyRequest().permitAll()
-//            .requestMatchers("/", "/login", "/oauth/**").permitAll()
-//            .anyRequest().authenticated()
-//            .and()
-//            .formLogin().permitAll()
-//            .and()
-//            .oauth2Login()
-//            .loginPage("/login")
-//            .userInfoEndpoint { oauthUserService }
-//            .userService(oauthUserService)
-//        return http.build()
-/*http
-    .authorizeRequests()
-    .anyRequest().authenticated()
-    .and()
-    .oauth2Login()
-    .authorizationEndpoint()
-    .baseUri("/oauth2/authorize-client")
-    .authorizationRequestRepository(authorizationRequestRepository())
-return http.build()*/
-//    }
-
-/*@Bean
-fun authorizationRequestRepository(): AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
-    return HttpSessionOAuth2AuthorizationRequestRepository()
-}*/
-
-/* @Bean
- fun userDetailsService(): InMemoryUserDetailsManager {
-     val user1: UserDetails = User.withUsername("user")
-         .password(passwordEncoder().encode("user"))
-         .roles("USER")
-         .build()
-     val admin: UserDetails = User.withUsername("admin1")
-         .password(passwordEncoder().encode("admin1"))
-         .roles("ADMIN", "USER")
-         .build()
-     return InMemoryUserDetailsManager(user1, admin)
- }
-
-
-
- @Bean
- fun webSecurityCustomizer(): WebSecurityCustomizer {
-     return WebSecurityCustomizer { web: WebSecurity ->
-         web.debug(true).ignoring()
-             .requestMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico")
-     }
- }
-
- @Bean
- @Throws(Exception::class)
- fun authenticationManager(
-     http: HttpSecurity,
-     bCryptPasswordEncoder: BCryptPasswordEncoder?,
-     userDetailService: InMemoryUserDetailsManager?
- ): AuthenticationManager {
-     return http.getSharedObject(AuthenticationManagerBuilder::class.java)
-         .userDetailsService(userDetailService)
-         .passwordEncoder(bCryptPasswordEncoder)
-         .and()
-         .build()
- }
-
- @Bean
- @Throws(Exception::class)
- fun filterChain(http: HttpSecurity): SecurityFilterChain {
-     http
-//            .csrf { it.disable() }
-         .authorizeHttpRequests {
-             it
-//                    .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-//                    .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-//                    .requestMatchers("/pets*").hasAnyRole("USER", "ADMIN")
-//                    .requestMatchers("/login")
-                 .requestMatchers("/pets")
-                 .permitAll()
-                 .anyRequest().authenticated()
-//                     .requestMatchers("/secured").permitAll()
-//                    .requestMatchers("/", "/login", "/logout", "/logout-success").permitAll()
-//                    .requestMatchers( "/checkout", "/pets", "/search", "/secured").authenticated()
-//                    .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-//                    .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-//                    .anyRequest().authenticated()
-         }
-         .formLogin {
-             it.loginPage("/login")
-//                    .loginProcessingUrl("/dologin")
-                 .failureUrl("/login?error=true")
-                 .defaultSuccessUrl("/pets", true)
-                 .permitAll()
-         }
-//            .logout {
-//                it.invalidateHttpSession(true).logoutSuccessUrl("/logout-success").deleteCookies("remove")
-//                    .logoutSuccessHandler(logoutSuccessHandler())
-//            }
-         .httpBasic(Customizer.withDefaults())
-         .sessionManagement {
-             it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-         }
-
-     return http.build()
- }
-
-
-}*/
