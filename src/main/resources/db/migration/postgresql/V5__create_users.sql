5f5df13638bdf3777369a9a0b2383a4dc7cb59fa
manos <emmanouil.psanis@gmail.com>
2025-03-26 13:54:58 +0100

    dev: update deployment target

--- src/main/resources/db/migration/postgresql/V5__create_users.sql
+++ src/main/resources/db/migration/postgresql/V5__create_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO app_user(account_non_expired, account_non_locked, credentials_non_expired, enabled, first_name, is_using2fa, last_name, password, provider, username) VALUES (true, true, true, true, 'Manos', false, 'Psanis', '$2a$11$JzcZs/MWVkna9YUCSD1hBOD7jiGcckq.VoXt3BxZ92IFRZVptJ8Je', 'LOCAL', 'emmanouil.psanis@gmail.com');
+INSERT INTO app_user(account_non_expired, account_non_locked, credentials_non_expired, enabled, first_name, is_using2fa, last_name, password, provider, username) VALUES (true, true, true, true, '4G37L9NcQDpDQlPEcygMIw==', false, 'MYTXiqFL+Oi474qJgJ34XA==', '$2a$11$l1pvQ9/IZqsyET5pzdAqo.NYUAFnDC027ugz040rYWwai1NHoMshS', 'GOOGLE', '2YfLiMkO/J3+cJQL3fjAAIZWL4c0bMzH3081eCB0NNQ=');
 
