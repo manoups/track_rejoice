28be975aa7c60dcdac3c6a9e40ee4c4d2542e512
manos <emmanouil.psanis@gmail.com>
2025-03-26 20:53:39 +0100

    dev: update sql file

--- src/main/resources/db/migration/postgresql/V6__link_users_to_roles.sql
+++ src/main/resources/db/migration/postgresql/V6__link_users_to_roles.sql
@@ -1,2 +1,2 @@
-INSERT INTO users_roles(user_id, role_id) VALUES ((select id from app_user where username='emmanouil.psanis@gmail.com'), (select id from role WHERE name='ROLE_ADMIN'));
+INSERT INTO users_roles(user_id, role_id) VALUES ((select id from app_user where username='2YfLiMkO/J3+cJQL3fjAAIZWL4c0bMzH3081eCB0NNQ='), (select id from role WHERE name='ROLE_ADMIN'));
 
